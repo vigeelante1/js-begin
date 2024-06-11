af31085270f9b1fadb26e3153c799d7853844e96
Vaibhav Sharma <91866722+vigeelante1@users.noreply.github.com>
2024-06-11 10:45:14 +0000

    conversions and operations

---
+++ .github/01_basics/03_conversionsOperations.js
@@ -0,0 +1,51 @@
+// let score = "33";
+// let score1 = "33abc"
+// let score2 = null; // 0
+// let score3 = undefined; // NAN
+// let score4 = true; // 1 
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// let valueInNumber1 = Number(score1)
+// let valueInNumber2 = Number(score2)
+// let valueInNumber3 = Number(score3)
+// let valueInNumber4 = Number(score4)
+// console.log(typeof (valueInNumber))
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)
+// console.log(valueInNumber4)
+
+// console.log(typeof NaN)
+
+/*
+    Conversion operations to Number;
+
+    "33" -> 33
+    "33abc" -> NaN
+    null -> 0
+    undefined -> NaN
+    true -> 1
+    false -> 0
+    "vaibhav" -> NaN
+
+*/
+
+// let loggedIn = 1
+
+// let booleanLoggedIn = Boolean(loggedIn)
+
+// console.log(typeof booleanLoggedIn)
+// console.log(booleanLoggedIn)
+
+// 1 -> true , 0 -> false
+
+let someNumber = 33;
+
+let someString = String(someNumber)
+
+console.log(someString)
+console.log(typeof someString)
