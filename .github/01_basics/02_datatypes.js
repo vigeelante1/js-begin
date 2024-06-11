a7b628eabcdd7a1ed494c629e03542d3ee4f5ce1
Vaibhav Sharma <91866722+vigeelante1@users.noreply.github.com>
2024-06-11 10:06:31 +0000

    datatypes

---
+++ .github/01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version;
+
+// alert(3 + 3) we are using nodejs , not browser;
+
+/*
+    Primitive Data Types.
+
+    number -> 2 to the power 53
+    bigint
+    string -> ""
+    boolean -> true/ false
+    null -> standalone value
+    undefined -> value is stil not assigned
+    symbol -> unique
+*/
+
+/*
+    Object
+
+
+
+
+
+*/
+
+console.log(typeof "vaibhav");
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
