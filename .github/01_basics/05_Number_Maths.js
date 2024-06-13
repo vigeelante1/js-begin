d477f767c1143670d3d725aef43be31b45001ccf
Vaibhav Sharma <91866722+vigeelante1@users.noreply.github.com>
2024-06-13 19:22:17 +0000

    Numbers and Maths

---
+++ .github/01_basics/05_Number_Maths.js
@@ -0,0 +1,8 @@
+const number = 100;
+
+const balance = new Number(100000)
+
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toPrecision(4)); // Before Decimal Value;
+console.log(balance.toLocaleString("en-IN")); // to represent 0's and en-IN represents in Indian Number System.
