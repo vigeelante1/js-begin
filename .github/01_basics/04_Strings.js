cc858a23ace123b443b02bdf4bb217d10a436404
Vaibhav Sharma <91866722+vigeelante1@users.noreply.github.com>
2024-06-13 17:08:58 +0000

    Strings Done

--- .github/01_basics/04_Strings.js
+++ .github/01_basics/04_Strings.js
@@ -5,7 +5,7 @@ const repo = 4;
 // console.log(`The name is ${name} and the repo count is ${repo}`); // valid form of writing;
 
 // different way of declaring String as;
-const name1 = new String("Vigeelante") // as an object you did it , in the constructor you write the value;
+const name1 = new String("Vigeelante-is-a-good-man") // as an object you did it , in the constructor you write the value;
 
 // console.log(name1);
 // console.log(name1[0]);
@@ -14,5 +14,22 @@ const name1 = new String("Vigeelante") // as an object you did it , in the const
 const newString = name1.substring(0 , 4)
 
 // console.log(newString);
-console.log(typeof (newString) === typeof (name1));
+// console.log(typeof (newString) === typeof (name1));
 // console.log(newString === name1);
+
+const newString1 = "    vaibhavJi " 
+
+// console.log(newString1);
+// console.log(newString1.trim()); // with the help of trim , all the extra spaces have been removed.
+
+const url = "https://vaibhav.com/vaibhav%20sharma"
+
+const newURL = url.replace("%20" , "-")
+// console.log(newURL);
+// console.log(url.replace("%20" , "-")); it replaces the given value to the original value
+
+// console.log(url);
+
+// console.log(url.includes("vaibhav")); it return true or false in case whether it includes the input given or NOT;
+
+console.log(name1.split("-"));
