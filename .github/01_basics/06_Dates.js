f8d4a33d743fb60faa10f279b01909e9f1545b78
Vaibhav Sharma <91866722+vigeelante1@users.noreply.github.com>
2024-06-13 20:12:26 +0000

    Dates in JS

---
+++ .github/01_basics/06_Dates.js
@@ -0,0 +1,13 @@
+// datesss
+
+let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toJSON());
+
+// console.log(typeof mydate);
+
+let myCreatedDate = new Date(2024 , 11 , 18 , 6 , 5);
+
+console.log(myCreatedDate.toLocaleString());
