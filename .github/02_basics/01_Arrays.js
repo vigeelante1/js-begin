63e445727e1a14c947068b8ba58ee813d14a3c4d
Vaibhav Sharma <91866722+vigeelante1@users.noreply.github.com>
2024-06-14 05:28:53 +0000

    Intro to Arrays

---
+++ .github/02_basics/01_Arrays.js
@@ -0,0 +1,17 @@
+// first way of declaring an array;
+const myArr = [0, 1, 2, 3];
+
+// second way of declaring an array;
+const myArr2 = new Array(1 , 2 , 4 , 5)
+
+// console.log(myArr[0]);
+// console.log(typeof myArr);
+// console.log(typeof myArr2);
+
+// methods of arrays;
+
+myArr.push(100);
+myArr.push(200);
+
+// console.table(myArr)
+
